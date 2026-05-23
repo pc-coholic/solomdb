45f3458212deba831d06018257044046aabd9b14
Martin Gross <martin@pc-coholic.de>
2026-05-23 16:15:53 +0200

    Split MDB-device into classes

---
+++ mdbdevices/waferstar.py
@@ -0,0 +1,5 @@
+from serial.threaded import LineReader
+
+
+class Waferstar(LineReader):
+    pass
--- solomdb.py
+++ solomdb.py
@@ -2,7 +2,6 @@
 import argparse
 import configparser
 import sys
-import time
 import uuid
 from decimal import Decimal, ROUND_HALF_UP
 from threading import Thread
@@ -10,8 +9,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import requests
 import serial
-from requests import HTTPError
-from serial.threaded import LineReader, ReaderThread
+from serial.threaded import ReaderThread
 
 
 class SoloMDB(object):
@@ -34,6 +32,16 @@ class SoloMDB(object):
             # Check if essential keys are present
             self.config.get('apikey')
             self.config.get('mdbdevice')
+
+            # Instantiate the underlying MDB device
+            if self.config.get('devicetype') == 'qibixx':
+                from mdbdevices.qibixx import Qibixx
+                self.mdb_device = Qibixx
+            elif self.config.get('devicetype') == 'waferstar':
+                from mdbdevices.waferstar import Waferstar
+                self.mdb_device = Waferstar
+            else:
+                raise Exception('Unknown MDB device type')
         except (configparser.NoSectionError, configparser.NoOptionError) as e:
             print(e)
             sys.exit()
@@ -153,7 +161,7 @@ class SoloMDB(object):
             timeout=1,
         )
 
-        self.mdb_thread = ReaderThread(self.serial, MDBLineReader)
+        self.mdb_thread = ReaderThread(self.serial, self.mdb_device)
 
     def start_mdb(self):
         self.mdb_thread.start()
@@ -163,142 +171,14 @@ class SoloMDB(object):
         self.mdb_thread.join()
 
     def stop_mdb(self):
-        self.mdb_thread.write('C,0\n'.encode('UTF-8'))
+        if hasattr(self.mdb_device, 'stop_mdb_command'):
+            self.mdb_thread.write(f'{self.mdb_device.stop_mdb_command}\n'.encode('UTF-8'))
+
         self.mdb_thread.close()
 
     def start_session(self):
-        self.mdb_thread.write('C,START,99.99\n'.encode('UTF-8'))
-
-
-class MDBLineReader(LineReader):
-    def __init__(self):
-        super().__init__()
-        self.solomdb = SoloMDB()
-        self.transport = None
-
-    def write_line(self, text: str) -> None:
-        sys.stdout.write('line sent: {}\n'.format(repr(text)))
-        return super().write_line(text)
-
-    def connection_made(self, transport):
-        super(MDBLineReader, self).connection_made(transport)
-        self.transport = transport
-        sys.stdout.write('port opened\n')
-        self.write_line('C,0')
-        self.write_line('C,SETCONF,mdb-currency-code=0x1978')
-        self.write_line('C,SETCONF,mdb-always-idle=1')
-        # self.write_line('X,1')
-        self.write_line('C,1')
-
-    def payment_thread(self):
-        while True:
-            try:
-                data = self.solomdb.get_payment(self.solomdb.payment_uuid)
-                self.solomdb.transaction_code = data.get('transaction_code')
-                payment_status = data.get('status')
-                payment_amount = data.get('amount')
-            except HTTPError as e:
-                print(f"Payment retrieval failed: {e}")
-            else:
-                print(f'Payment Status for {self.solomdb.payment_uuid} is {payment_status}')
-                match payment_status:
-                    case 'PENDING':
-                        if self.solomdb.should_cancel:
-                            print('Trying to cancel payment on reader')
-                            self.solomdb.cancel_payment()
-                        pass
-                    case 'FAILED' | 'CANCELLED':
-                        self.solomdb.clear_payment_status()
-                        return
-                    case 'SUCCESSFUL':
-                        if self.solomdb.mdb_status == 'VEND' and not self.solomdb.should_cancel:
-                            print('Machine in state VEND, approving vend')
-                            self.write_line(f'C,VEND,{payment_amount}')
-                            return
-                        else:
-                            print('Machine not in state VEND or cancellation is requested, refunding')
-                            self.solomdb.refund_thread = Thread(target=self.refund_thread, args=[self.solomdb.transaction_code]).start()
-                            self.solomdb.clear_payment_status()
-                            return
-            time.sleep(1)
-
-    def refund_thread(self, transaction_code: str):
-        print(f'Trying to refund transaction {transaction_code}')
-        while True:
-            try:
-                self.solomdb.refund_payment(self.solomdb.transaction_code)
-            except HTTPError as err:
-                if err.response.status_code == 409:
-                    print(f'Refund error 409 for {transaction_code}; probably already refunded.')
-                    break
-                else:
-                    print("Refund error, retrying...")
-                    time.sleep(1)
-            else:
-                break
-
-    def handle_line(self, data):
-        sys.stdout.write('line received: {}\n'.format(repr(data)))
-
-        cmd, payload = data.split(',', 1)
-        payload = payload.split(',')
-
-        match cmd:
-            case 'c':
-                match payload[0]:
-                    case 'SET':
-                        pass
-                    case 'ERR':
-                        match payload[1]:
-                            case 'VEND 1':
-                                if self.solomdb.payment_uuid is not None:
-                                    self.solomdb.should_cancel = True
-                            # Fixme c,ERR,VEND 3...
-                            case _:
-                                print("An error occurred, stopping interface")
-                                self.write_line('C,0')
-                    case 'STATUS':
-                        self.solomdb.mdb_status = payload[1]
-                        match payload[1]:
-                            case 'VEND':
-                                amount = Decimal(payload[2])
-                                print(f'Requesting payment of {amount}')
-                                if self.solomdb.payment_uuid is not None:
-                                    print(f'Previous payment_uuid {self.solomdb.payment_uuid} present; not charging again')
-
-                                    # Same price, we can reuse the payment
-                                    if Decimal(amount) == Decimal(self.solomdb.vend_amount):
-                                        self.solomdb.should_cancel = False
-                                else:
-                                    if amount < Decimal('1.0'):
-                                        print('Ignoring request for amount < 1.00 EUR')
-                                    else:
-                                        self.solomdb.vend_amount = amount
-                                        self.solomdb.start_payment(amount)
-                                        Thread(target=self.payment_thread).start()
-                            case 'IDLE':
-                                # Should stop and refund payment
-                                if self.solomdb.payment_uuid:
-                                    self.solomdb.should_cancel = True
-                            case 'DISABLED':
-                                # Should stop and refund payment
-                                if self.solomdb.payment_uuid:
-                                    self.solomdb.should_cancel = True
-                    case 'VEND':
-                        if payload[1] == 'SUCCESS':
-                            print("Payment successful, Distribution successful")
-                            self.solomdb.clear_payment_status()
-            case 'r':
-                pass
-
-            case 'x':
-                pass
-
-    def connection_lost(self, exc):
-        #if exc:
-        #    traceback.print_exc(exc)
-        print(exc)
-        sys.stdout.write('port closed\n')
+        if hasattr(self.mdb_device, 'start_session_command'):
+            self.mdb_thread.write(f'{self.mdb_device.start_session_command}\n'.encode('UTF-8'))
 
 
 class RequestHandler(BaseHTTPRequestHandler):
