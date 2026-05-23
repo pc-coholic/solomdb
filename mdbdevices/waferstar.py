35eb46949dd05a26e9d4600d041c6f59520cc17b
robbi5 <maxi@richt.name>
2026-05-23 21:59:13 +0200

    Move to hex values, add a bit more commands

--- mdbdevices/waferstar.py
+++ mdbdevices/waferstar.py
@@ -1,34 +1,46 @@
+import codecs
 import sys
 
-from serial.threaded import LineReader, Packetizer, FramedPacket
+from serial.threaded import FramedPacket
 
 from solomdb import SoloMDB
 
 
 class Waferstar(FramedPacket):
-    START = b'\x02'
-    STOP = b'\x03'
+    START = b"\x02"
+    STOP = b"\x03"
 
     def __init__(self):
         super().__init__()
         self.solomdb = SoloMDB()
         self.transport = None
 
-    def write_line(self, text: str) -> None:
-        sys.stdout.write('line sent: {}\n'.format(repr(text)))
-        return super().write_line(text)
-
     def connection_made(self, transport):
         super(Waferstar, self).connection_made(transport)
         self.transport = transport
-        sys.stdout.write('port opened\n')
+        sys.stdout.write("port opened\n")
         # ToDo: Setup-Code here
 
     def handle_packet(self, packet):
-        sys.stdout.write('packet received: {}\n'.format(repr(packet)))
+        sys.stdout.write("packet received: {}\n".format(repr(packet)))
 
-        payload = [packet[i:i+2] for i in range(0, len(packet), 2)]
+        payload = []
+        try:
+            payload = [
+                codecs.decode(packet[i : i + 2], "hex")
+                for i in range(0, len(packet), 2)
+            ]
+        except Exception as e:
+            print("Got broken packet")
+            print(e)
+            return
+
         cmd = payload.pop(0)
+
+        if len(payload) == 0 and cmd in (b"\x00",):
+            print("Okay.")
+            return
+
         if len(payload) > 1:
             subcmd = payload.pop(0)
         else:
@@ -38,64 +50,124 @@ class Waferstar(FramedPacket):
         # http://www.mdbprotocol.com/NAMA/Mdb_protocol.pdf
         # page 125
         match cmd:
-            case b'01':
+            case b"\x01":
                 print("Current config data")
                 self.setup_config_data()
-            case b'09':
-                print("Current Max/Min Prices")
-            case b'10':  # Reset
+            case b"\x09":
+                print("Current Max/Min Prices / Ident")
+                # self.setup_ident()
+            case b"\x10":  # Reset
                 print("Reset")
-            case b'11':  # Setup
+            case b"\x11":  # Setup
                 match subcmd:
-                    case b'00':
+                    case b"\x00":
                         print("Config Data")
-                    case b'01':
+                    case b"\x01":
                         print("Max/Min Prices")
-            case b'12':  # Poll
+                        # can be ignored, is auto ACKed
+            case b"\x12":  # Poll
                 print("Polling (should never be posted here)")
-            case b'13':  # Vend
+            case b"\x13":  # Vend
                 print("Vend")
-            case b'14':  # Reader
+            case b"\x14":  # Reader
                 match subcmd:
-                    case b'00':
+                    case b"\x00":
                         print("Reader disabled")
-                    case b'01':
+                    case b"\x01":
                         print("Reader enabled")
+                    case b"\x02":
+                        print("Reader cancel")
+                        self.send_command([0x08])
                     case _:
                         print("Unchecked Reader subcommand")
-            case b'15':  # Revalue
+            case b"\x15":  # Revalue
                 print("Revalue")
-            case b'17':  # Expansion
+            case b"\x17":  # Expansion
                 print("Expansion")
+                match subcmd:
+                    case b"\x00":
+                        print("Request ID")
+                        self.setup_ident()
+                    case b"\x04":
+                        print("Expansion Enable Feature command")
+                        # needs no answer?
+                    case _:
+                        print("Unchecked Expansion subcommand")
             case _:
                 print("Unchecked Command")
 
     def crc(self, command):
-        return str(sum([int(x) for x in command])).encode()
+        return sum(command) & 0xFF
 
     def send_command(self, command):
-        self.write_line(command + [self.crc(command)])
+        complete = command + [self.crc(command)]
+        text = bytes(complete).hex()
+        sys.stdout.write("line sent: {}\n".format(repr(text)))
+        self.transport.write(complete)
 
     def setup_config_data(self):
-        self.send_command([
-            b'01',  # Reader Config Data
-            b'03',  # Reader Feature Level
-            b'19',  # Country Code
-            b'78',  # Country Code
-            b'01',  # Scale Factor
-            b'02',  # Decimal places
-            b'07',  # Application maximum response time (seconds)
-            b'01',  # Misc options (Supports VEND/CASH Subcommand)
-        ])
+        self.send_command(
+            [
+                0x01,  # Reader Config Data
+                0x03,  # Reader Feature Level
+                0x19,  # Country Code
+                0x78,  # Country Code
+                0x01,  # Scale Factor
+                0x02,  # Decimal places
+                0x07,  # Application maximum response time (seconds)
+                0x01,  # Misc options (Supports VEND/CASH Subcommand)
+            ]
+        )
+
+    def setup_ident(self):
+        self.send_command(
+            [
+                0x09,  # Respond
+                0x49,
+                0x44,
+                0x53,  # Manufacturer ID
+                0x30,
+                0x30,
+                0x30,
+                0x30,
+                0x31,
+                0x30,
+                0x30,
+                0x30,
+                0x36,
+                0x36,
+                0x30,
+                0x32,  # Serial Number
+                0x49,
+                0x44,
+                0x53,
+                0x4D,
+                0x44,
+                0x42,
+                0x56,
+                0x4D,
+                0x43,
+                0x43,
+                0x4F,
+                0x4E,  # Model Number
+                0x30,
+                0x32,  # Software Version
+                0x00,
+                0x00,
+                0x00,
+                0x20,  # Feature Flags
+                # 0x13 checksum
+            ]
+        )
 
     def setup_min_max_prices(self):
         pass
 
     def handle_out_of_packet_data(self, data):
-        sys.stdout.write('out_of_packet_data: {}\n'.format(repr(data)))
+        sys.stdout.write("out_of_packet_data: {}\n".format(repr(data)))
 
     def connection_lost(self, exc):
-        #if exc:
+        # if exc:
         #    traceback.print_exc(exc)
         print(exc)
-        sys.stdout.write('port closed\n')
+        sys.stdout.write("port closed\n")
